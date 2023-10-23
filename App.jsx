d41a19c08a92f5f52038d95e9f1b5a19fa1b8e70
Vladimir Hanhur <aqgz@email.cz>
2023-10-23 17:37:29 +0200

    update

--- App.jsx
+++ App.jsx
@@ -1,30 +1,60 @@
 import 'react-native-gesture-handler';
+import React, { useCallback, useEffect, useState } from 'react';
+import { ActivityIndicator, View } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
-import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+import * as Font from 'expo-font';
 import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
-import { store, persistor } from './redux/store';
-import { Main } from './components/Main';
+import { store } from './src/redux/store';
+import Main from './src/components/Main/Main';
 
-export default function App() {
-    const [fontsLoaded] = useFonts({
-        'Roboto-Regular': require('./assets/fonts/Roboto-Regular.ttf'),
-        'Roboto-Medium': require('./assets/fonts/Roboto-Medium.ttf'),
+export default function App() 
+{
+    const [appIsReady, setAppIsReady] = useState(false);
+
+    useEffect(() => {
+        async function prepare() 
+        {
+            try 
+            {
+                await Font.loadAsync({
+                    'Roboto-Regular': require('./src/fonts/Roboto-Regular.ttf'),
+                    'Roboto-Medium': require('./src/fonts/Roboto-Medium.ttf'),
+                    'Roboto-Bold': require('./src/fonts/Roboto-Bold.ttf'),
+                });
+            } 
+            catch (error) 
+            {
+                console.warn('Error loading assets:', error);
+            } 
+            finally 
+            {
+                setAppIsReady(true);
+            }
+        }
+        prepare();
+    }, []);
+
+    const onLayoutRootView = useCallback(async () => {
+        if (appIsReady) 
+        {
+            await SplashScreen.hideAsync();
+        }
     });
 
-    if (!fontsLoaded) 
+    if (!appIsReady) 
     {
-        return null;
+        return (
+            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+                <ActivityIndicator size="large" color="#0000ff" />
+            </View>
+        );
     }
 
     return (
-        <>
+        <Provider store={store}>
             <StatusBar style="auto" />
-            <Provider store={store}>
-                <PersistGate persistor={persistor}>
-                    <Main />
-                </PersistGate>
-            </Provider>
-        </>
+            <Main onLayout={onLayoutRootView} />
+        </Provider>
     );
 }
---
+++ src/firebase/config.jsx
@@ -0,0 +1,20 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
+
+const firebaseConfig = {
+    apiKey: 'AIzaSyDGFJo7XBSTSpIsdzyG9qiAES3N630LdVY',
+    authDomain: 'native-v2-3fc9a.firebaseapp.com',
+    databaseURL: 'https://native-v2-3fc9a-default-rtdb.europe-west1.firebasedatabase.app',
+    projectId: 'native-v2-3fc9a',
+    storageBucket: 'native-v2-3fc9a.appspot.com',
+    messagingSenderId: '172387015447',
+    appId: '1:172387015447:web:83ea4b3500d081df613552',
+    measurementId: 'G-SWZTDG1C1N',
+}
+
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
